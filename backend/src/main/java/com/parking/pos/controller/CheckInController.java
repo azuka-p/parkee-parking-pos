e58bebf3a33c6e659c348bc2ed4eba2cde4f3246
azuka-p <rad.azka.16@gmail.com>
2025-02-15 17:27:16 +0700

    fix: fix cors

--- backend/src/main/java/com/parking/pos/controller/CheckInController.java
+++ backend/src/main/java/com/parking/pos/controller/CheckInController.java
@@ -8,15 +8,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.temporal.ChronoUnit;
 
 @RestController
 @RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin(origins = "*")
 public class CheckInController {
 
     @Autowired
