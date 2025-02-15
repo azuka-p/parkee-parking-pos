de8f3658134db5dc71a8628f380bedaf584285a2
azuka-p <rad.azka.16@gmail.com>
2025-02-15 17:30:46 +0700

    chore: tidy up checkout page

--- frontend/src/pages/TicketCheckoutPage.tsx
+++ frontend/src/pages/TicketCheckoutPage.tsx
@@ -1,8 +1,5 @@
 import {Camera} from "../components/Camera/Camera.tsx";
 import styled from "styled-components";
-// import {CheckInData, CheckInForm} from "../components/CheckInput/CheckInForm.tsx";
-// import {useState} from "react";
-// import {TicketInForm} from "../components/TicketInput/TicketInForm.tsx";
 
 const CameraContainer = styled.div`
   margin: 5px;
@@ -15,13 +12,8 @@ export const TicketCheckOutPage = () => {
   return (
     <>
       <CameraContainer>
-        <Camera id={1} label={`AAAAAAA`}/>
-        <Camera id={2} label={`AAAAAAA`}/>
-        <Camera id={3} label={`AAAAAAA`}/>
-        <Camera id={4} label={`AAAAAAA`}/>
+        <Camera id={1} label={`Kamera 1`}/>
       </CameraContainer>
-      {/*<CheckInForm data={checkInData} handleChange={(data: CheckInData) => setCheckInData(data)}/>*/}
-      {/*<TicketInForm data={checkInData} reset={() => setCheckInData({} as CheckInData)}/>*/}
     </>
   );
 };
